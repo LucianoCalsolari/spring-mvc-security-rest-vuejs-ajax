e4df58c99add86b4ec3c38de40766352257d8202
LucianoCalsolari <souzaluciano9517@yahoo.com.br>
2020-12-10 21:38:58 -0300

    Recuperando valores de oferta e criando ofertas

--- src/main/java/br/com/alura/mvc/mudi/controller/UsuarioController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/UsuarioController.java
@@ -4,8 +4,6 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,32 +15,32 @@ import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.model.StatusPedido;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
-@Controller
-@RequestMapping("/home")
-public class HomeController {
+@Controller	
+@RequestMapping("usuario")
+public class UsuarioController {
+
 	
 	@Autowired
 	private PedidoRepository repository;
 	
-	@GetMapping()
+	@GetMapping("pedido")
 	public String home(Model model, Principal principal) {
-		List<Pedido> pedidos = repository.findAll();
+		List<Pedido> pedidos = repository.findAllByUsuario(principal.getName());
 		model.addAttribute("pedidos", pedidos);
-		return "home"; 
+		return "usuario/home"; 
 	}
 	
-	@GetMapping("/{status}")
-	public String porStatus(@PathVariable("status") String status, Model model) {
-		
-		Sort sort = Sort.by("dataDaEntrega").descending();
-		PageRequest paginacao = PageRequest.of(0, 1,sort);
-		
-
-		List<Pedido> pedidos = repository.findByStatus(StatusPedido.ENTREGUE, paginacao);
+	@GetMapping("pedido/{status}")
+	public String porStatus(@PathVariable("status") String status, Model model, Principal principal) {
+		List<Pedido> pedidos = repository.findByStatusEUsuario(StatusPedido.valueOf(status.toUpperCase()), principal.getName());
 		model.addAttribute("pedidos", pedidos);
-		
-		return "home"; 
+		model.addAttribute("status", status);
+		return "usuario/home"; 
 	}
 	
-
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/usuario/home";
+	}
+	
 }
